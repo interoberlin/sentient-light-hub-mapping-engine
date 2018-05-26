a11910c44db67e1910f2602aa50e834ef31da27e
Florian Schwanz <florian.schwanz@interoberlin.de>
2018-05-26 21:06:03 +0200

    Enabled scheduling

--- src/main/kotlin/berlin/intero/sentientlighthub/mappingengine/App.kt
+++ src/main/kotlin/berlin/intero/sentientlighthub/mappingengine/App.kt
@@ -2,9 +2,11 @@ package berlin.intero.sentientlighthub.mappingengine
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 import java.util.logging.Logger
 
 @SpringBootApplication
+@EnableScheduling
 class App
 
 fun main(args: Array<String>) {
